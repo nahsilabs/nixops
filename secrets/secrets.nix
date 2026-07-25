0bd69018bfc24d232e0a104683f917d918f1a9fd
Anatolios Laskaris <github_me@nahsi.dev>
2026-07-26 00:48:57 +0300

    Rekey VM secret after cloud-init update

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  vmTest = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/IfikldCcM6glbgq7vnm+TLfdYHjlY7uRjsahaUL0v";
+  vmTest = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOTa+mggzTikbU3K87Ncrd3E5tVC2U8MJ2js7ysNl4g";
   recovery = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgUMXDS82IxVYcp82we7V9dQCQgErQVPZAeNa0Ve6AK";
 in
 {
